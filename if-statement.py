6f0b24e27f8e50c6f4986c36b74401823481c2dc
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-03-10 13:53:50 +0200

    If statements

---
+++ if-statement.py
@@ -0,0 +1,39 @@
+# age = int(input("How old are you?"))
+
+# if age == 100:
+#   print("You are a century old")
+# elif age >= 18:
+#   print("You are an adult")
+# elif age < 0:
+#   print("You haven't been born yet")
+# else:
+#   print("You are a child")
+
+  #---------------------------------
+
+
+# response = input("Would you like food? (Y/N)")
+
+# if response == "Y":
+#   print("Have some food")
+# else:
+#   print("No food for you")
+
+#   #--------------------------------------------
+
+#   name = input("Enter your name: ")
+
+#   if name == "":
+#     print("You didn't enter your name")
+#   else:
+#     print(f"Hello{name}")
+
+
+#--------------------------------------------
+online = False
+
+if online:
+  print("The user is online")
+    
+else:
+  print("The user is offline")  
