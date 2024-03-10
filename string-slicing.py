57e2ac871236511fb3871ddddbee0223e5b0fa61
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-03-10 14:21:15 +0200

    String slicing updated

--- string-slicing.py
+++ string-slicing.py
@@ -2,21 +2,44 @@
 #another string indexing[] or slice()
 #[start:stop:step]
 
-name = "Bro Code"
+# name = "Bro Code"
 
-# first_name = name[0:3]
-first_name = name[:3]
-#the last letter is the last index + 1
-# last_name = name[4:8]
-last_name = name[:8]
-# funky_name = name[0:8:2] #every x character including the first (depends the last value)
-funky_name = name[::2]
-reversed_name = name[::-1]
+# # first_name = name[0:3]
+# first_name = name[:3]
+# #the last letter is the last index + 1
+# # last_name = name[4:8]
+# last_name = name[:8]
+# # funky_name = name[0:8:2] #every x character including the first (depends the last value)
+# funky_name = name[::2]
+# reversed_name = name[::-1]
 
-print(first_name)
+# print(first_name)
 
-website = "http://google.com"
-website2 = "http://wikipedia.com"
-slice = slice(7, -4)
+# website = "http://google.com"
+# website2 = "http://wikipedia.com"
+# slice = slice(7, -4)
 
-print(website2[slice])
+# print(website2[slice])
+
+#------------------------------------------
+credit_number = "1234-5678-9012-3456"
+
+print(credit_number[0])
+print(credit_number[0:4])
+print(credit_number[:4])
+print(credit_number[4:8])
+print(credit_number[4:])
+print(credit_number[-1])
+print(credit_number[-4:])
+print(credit_number[::2])
+print(credit_number[::3])
+
+# EXERCISE 1
+credit_number = "1234-5678-9012-3456"
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+# EXERCISE 2
+credit_number = "1234-5678-9012-3456"
+credit_number = credit_number[::-1]
+print(credit_number)
