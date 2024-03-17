1adab8caf135aade0ee42fa089b2dc834f633dce
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-03-17 12:35:18 +0200

    Break-continue-pass

---
+++ break-continue-pass.py
@@ -0,0 +1,26 @@
+#break = terminate the loop entirely
+#continue = skip to the next iteration
+#pass = does nothing, acts like a placeholder
+
+while True:
+  name = input("Enter your name")
+  if name != "":
+    break
+
+
+phone_number = 123-456-7890
+
+for i in phone_number:
+  if i == "-":
+    continue
+  print(i, end="")
+  
+
+
+for i in range(1, 21): 
+  if i == 13:
+    pass
+  else:
+    print(i)
+
+#21 is exclusive, so will loop through 20
