e88b4135d79dbab5e1ee04c2f6764686579a8510
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-03-17 13:59:16 +0200

    Lists updated

--- lists.py
+++ lists.py
@@ -1,3 +1,6 @@
+#   List  = [] ordered and changeable. Duplicates OK
+
+
 foods = ["pizza", "hamburger", "hotdog", "spaghetti", "pudding"]
 
 foods[0] = "sushi"
@@ -13,7 +16,11 @@ foods[0] = "sushi"
 #foods.clear() #remove all the elements of the list
 #foods.reverse() #to reverse
 
-print(foods.index("pizza"))
+# print(foods.index("pizza")) #return an index
+# foods.count("pizza") #how many of pizza are in the array
 
+# print(dir(foods)) #return a list of attributes and methods of an object
+# print(help(foods))
+# print(len(foods))
 for food in foods:
   print(food)
